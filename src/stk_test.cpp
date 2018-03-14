162f35195958264b37a937421dfb6f2fd7440176
Luke <luke4775@gmail.com>
2018-03-14 16:00:37 -0700

    Real time convolving with audio

--- src/stk_test.cpp
+++ src/stk_test.cpp
@@ -18,9 +18,17 @@ using namespace stk;
 unordered_map<int, vector<Fir> > hrtf_set;
 
 void conv_audio(FileWvIn& input, FileWvOut& output, int angle, StkFrames& iframes, StkFrames& oframes) {
+  size_t padding = 1000;
+  // iframes.resize(iframes.size() + padding);
+  // oframes.resize(oframes.size() + padding, 2);
+  // cout << "iframes size: " << iframes.size() << endl;
   input.tick(iframes, 0);
+  // cout << "First" << endl;
   hrtf_set[angle][1].tick(iframes, oframes, 0, 1);
-  hrtf_set[angle][0].tick(iframes, oframes, 0, 0);  
+  hrtf_set[angle][0].tick(iframes, oframes, 0, 0);
+  // oframes.resize(oframes.size() - padding, 2);
+  // iframes.resize(iframes.size() - padding);
+  // input.addTime(-1 * padding);
   output.tick(oframes);
 }
 
@@ -53,9 +61,11 @@ int main()
 
   input.openFile( "despacito.wav", false );
   StkFloat sampleRate = input.getFileRate();
+  unsigned int channels = input.channelsOut();
   unsigned long sampleSize = input.getSize();
   cout << "Number of Sample points: " << sampleSize << endl;
   cout << "SampleRate: " << sampleRate << endl;
+  cout << "channels: " << channels << endl;
   cout << "Time: " << sampleSize/sampleRate << endl;
 
   
@@ -88,21 +98,38 @@ int main()
   Fir lfilter_(leftcoefficients_);
 
   
-  // Open a 16-bit, one-channel WAV formatted output file
-  output.openFile( "hellosine.wav", 2, FileWrite::FILE_WAV, Stk::STK_SINT16 );
+  // Open a 16-bit, two-channel WAV formatted output file
+  output.openFile( "despacito_conv.wav", 2, FileWrite::FILE_WAV, Stk::STK_SINT16 );
   // input.setFrequency( 440.0 );
   // Run the oscillator for 40000 samples, writing to the output file
-  StkFrames iframes(sampleRate/2, 1);
+
+  RtWvOut *dac = 0;
+  try {
+    // Define and open the default realtime output device for two-channel playback
+    dac = new RtWvOut( 2 );
+  }
+  catch ( StkError & ) {
+    exit( 1 );
+  }
+
+
+  StkFrames iframes(sampleRate/2, channels);
   StkFrames oframes(sampleRate/2, 2);
 
-  for (int angle = 0; angle < 225; angle += 15) {
+  for (int angle = 0; angle < 360; angle += 15) {
     conv_audio(input, output, angle, iframes, oframes);
+    dac->tick(oframes);
     // conv_audio(input, output, 240, iframes, oframes);
     // conv_audio(input, output, 210, iframes, oframes);
   }
   
+  cout << "Finish" << endl;
+  dac->stop();
+  
+  cout << "Delete" << endl;
 
-
+  input.closeFile();
+  cout << "HERE" << endl;
  //  for ( int i=0; i<200000; i++ ) {
  //    output.tick( input.tick() );
 	// }
